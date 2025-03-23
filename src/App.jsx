4421b091ed20e7519ad2f7cc73ea9c2c648d221d
Kelson Leandro <79779398+Kelson-leo@users.noreply.github.com>
2025-03-23 21:22:09 +0000

    callBack - compartilhando estados entre componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
-  const [text, setText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    let t = event.target.value;
-    setText(t);
-  };
-
-  function addItem(event) {
-    event.preventDefault();
-    if(text) {
-      setItems([...items, text]);
-      setText("");
-    }    
+  function onAddItem(item) {
+    setItems([...items, item])
   };
 
   return (<div className='container'>
       <h1>Todo List</h1>
-      <form>
-        <input onChange={handleChange} type='text' value={text}></input>
-        <button onClick={addItem}>Adicionar</button>
-        </form>
+        <TodoForm onAddItem={onAddItem}></TodoForm>
         <List items={items}></List>
     </div>
   );
@@ -35,6 +21,30 @@ function List(props) {
       <ul>
           {props.items.map(item => <li>{item}</li>)}
       </ul>
+  )
+}
+
+function TodoForm(props) {
+  const [text, setText] = useState("");
+
+  function handleChange(event) {
+    let t = event.target.value;
+    setText(t);
+  };
+
+  function addItem(event) {
+    event.preventDefault();
+    if(text) {
+      props.onAddItem(text);
+      setText("");
+    }    
+  };
+
+  return(
+    <form>
+      <input onChange={handleChange} type='text' value={text}></input>
+      <button onClick={addItem}>Adicionar</button>
+    </form>
   )
 }
 
